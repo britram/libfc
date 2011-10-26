f5858061fdd4ddf8a082792a521a6c0386c57456
Brian Trammell <trammell@tik.ee.ethz.ch>
2011-10-26 10:54:28 +0000

    more docs -- conditional compilation in Msg for IPFIX
    git-svn-id: https://minerva.netgroup.uniroma2.it/svn/demons/Sources/blockmon/main/node/lib/fc@1467 56ad3299-f7c0-456c-8eb5-fe9b43863049

--- Constants.h
+++ Constants.h
@@ -1,3 +1,13 @@
+/** 
+ * @file
+ * @author Brian Trammell <trammell@tik.ee.ethz.ch>
+ *
+ * @section DESCRIPTION
+ * 
+ * Defines constants used in libfc
+ */
+ 
+
 #ifndef IPFIX_K_H // idem
 #define IPFIX_K_H // hack
 
@@ -5,17 +15,27 @@
 
 namespace IPFIX {
     
+    /** IPFIX framing constant: message header version */
     static const size_t kIpfixVersion = 10;
+    /** IPFIX framing constant: message header length */
     static const size_t kMessageHeaderLen = 16;
+    /** IPFIX framing constant: set header length */
     static const size_t kSetHeaderLen = 4;
+    /** IPFIX framing constant: offset into set header of set length field */
     static const size_t kSetLenOffset = 2;
+    /** IPFIX framing constant: length of template record header */
     static const size_t kTemplateHeaderLen = 4;
     
+    /** Maximum length of an IPFIX message */
     static const size_t kMaxMessageLen = 65535;
+    /** Maximum length of an IPFIX set */
     static const size_t kMaxSetLen = 65519;
     
+    /** Set ID for template sets */
     static const uint16_t kTemplateSetID = 2;
+    /** Set ID for options template sets */
     static const uint16_t kOptionTemplateSetID = 3;
+    /** Minimum Set ID for data sets */
     static const uint16_t kMinSetID = 0x0100;
 
     /** The special IE length value signifying a variable-length IE */
