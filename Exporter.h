d9c0ce1078dad80b41d36870123dff45a831982c
Brian Trammell <trammell@tik.ee.ethz.ch>
2012-07-04 13:16:20 +0000

    work in progress commit -- adding bridge code for packet and flow, tweaks to libfc.
    git-svn-id: https://minerva.netgroup.uniroma2.it/svn/demons/Sources/blockmon/main/node/lib/fc@2914 56ad3299-f7c0-456c-8eb5-fe9b43863049

--- Exporter.h
+++ Exporter.h
@@ -179,7 +179,11 @@ public:
    */
   
     bool putValue(const InfoElement* ie, const void* vp, size_t len);
-  
+
+    bool putValue(const InfoElement* ie, uint8_t& v) {
+        return putValue(ie, &v, sizeof(v));
+    }
+
     bool putValue(const InfoElement* ie, uint8_t& v) {
         return putValue(ie, &v, sizeof(v));
     }
@@ -195,11 +199,11 @@ public:
     bool putValue(const InfoElement* ie, uint64_t& v) {
         return putValue(ie, &v, sizeof(v));
     }
-  
+
     bool putValue(const InfoElement* ie, std::string& v) {
         return putValue(ie, v.c_str(), v.size());
     }
-  
+
   /**
    * Flush the current message with the given export time
    *
