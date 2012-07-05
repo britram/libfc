d9b571e2f129e8aa076052a79c5252b98b964241
Stephan Neuhaus <neuhaust@tik.ee.ethz.ch>
2012-07-05 16:20:28 +0000

    IPv6 transcoding
    git-svn-id: https://minerva.netgroup.uniroma2.it/svn/demons/Sources/blockmon/main/node/lib/fc@2951 56ad3299-f7c0-456c-8eb5-fe9b43863049

--- test/TestTranscoder.cpp
+++ test/TestTranscoder.cpp
@@ -36,6 +36,14 @@ BOOST_AUTO_TEST_CASE(BufferManagement) {
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
 
+static uint32_t r32() {
+  static uint32_t seed = 12345678;
+
+  return seed = 69069*seed + 314159269;
+}
+
+static const unsigned int n_ipv4_transcode_tests = 1000;
+
 BOOST_AUTO_TEST_SUITE(Transcoder)
 
 BOOST_AUTO_TEST_CASE(TranscodeIPv4Address) {
@@ -44,23 +52,66 @@ BOOST_AUTO_TEST_CASE(TranscodeIPv4Address) {
   const IPFIX::InfoElement* ie_sip = m.lookupIE("sourceIPv4Address");
   BOOST_CHECK(ie_sip != 0);
 
-  uint8_t ipbuf[4] = { 1, 0, 0, 127 };
-  uint8_t trbuf[4];
-  IPFIX::Transcoder tc;
+  for (unsigned int i = 0; i < n_ipv4_transcode_tests; i++) {
+    uint32_t ip = r32();
+    uint8_t* ipbuf = reinterpret_cast<uint8_t*>(&ip);
+    uint8_t trbuf[4];
+    IPFIX::Transcoder tc;
 
-  tc.setBase(trbuf, sizeof(trbuf));
-  tc.zero();
+    tc.setBase(trbuf, sizeof(trbuf));
+    tc.zero();
 
-  BOOST_CHECK_EQUAL(tc.encode(&ipbuf[0], sizeof(ipbuf), ie_sip), 
-                    true);
+    BOOST_CHECK_EQUAL(tc.encode(&ipbuf[0], sizeof(ipbuf), ie_sip), true);
 
-  BOOST_CHECK_EQUAL(trbuf[0], 127);
-  BOOST_CHECK_EQUAL(trbuf[1], 0);
-  BOOST_CHECK_EQUAL(trbuf[2], 0);
-  BOOST_CHECK_EQUAL(trbuf[3], 1);
+    BOOST_CHECK_EQUAL(trbuf[0], ipbuf[3]);
+    BOOST_CHECK_EQUAL(trbuf[1], ipbuf[2]);
+    BOOST_CHECK_EQUAL(trbuf[2], ipbuf[1]);
+    BOOST_CHECK_EQUAL(trbuf[3], ipbuf[0]);
 
-  BOOST_CHECK_EQUAL(tc.avail(), 0);
-  BOOST_CHECK_EQUAL(tc.len(), sizeof(trbuf));
+    BOOST_CHECK_EQUAL(tc.avail(), 0);
+    BOOST_CHECK_EQUAL(tc.len(), sizeof(trbuf));
+  }
+}
+
+static const unsigned int n_ipv6_transcode_tests = 1;
+
+BOOST_AUTO_TEST_CASE(TranscodeIPv6Address) {
+  IPFIX::InfoModel m = IPFIX::InfoModel::instance();
+  m.defaultIPFIX();
+  const IPFIX::InfoElement* ie_sip = m.lookupIE("sourceIPv6Address");
+  BOOST_CHECK(ie_sip != 0);
+
+  for (unsigned int i = 0; i < n_ipv6_transcode_tests; i++) {
+    uint32_t ip[4] = { r32(), r32(), r32(), r32() };
+    uint8_t* ipbuf = reinterpret_cast<uint8_t*>(&ip);
+    uint8_t trbuf[16];
+    IPFIX::Transcoder tc;
+
+    tc.setBase(trbuf, sizeof(trbuf));
+    tc.zero();
+
+    BOOST_CHECK_EQUAL(tc.encode(&ipbuf[0], sizeof(ipbuf), ie_sip), true);
+    
+    BOOST_CHECK_EQUAL(trbuf[ 0], ipbuf[ 0]);
+    BOOST_CHECK_EQUAL(trbuf[ 1], ipbuf[ 1]);
+    BOOST_CHECK_EQUAL(trbuf[ 2], ipbuf[ 2]);
+    BOOST_CHECK_EQUAL(trbuf[ 3], ipbuf[ 3]);
+    BOOST_CHECK_EQUAL(trbuf[ 4], ipbuf[ 4]);
+    BOOST_CHECK_EQUAL(trbuf[ 5], ipbuf[ 5]);
+    BOOST_CHECK_EQUAL(trbuf[ 6], ipbuf[ 6]);
+    BOOST_CHECK_EQUAL(trbuf[ 7], ipbuf[ 7]);
+    BOOST_CHECK_EQUAL(trbuf[ 8], ipbuf[ 8]);
+    BOOST_CHECK_EQUAL(trbuf[ 9], ipbuf[ 9]);
+    BOOST_CHECK_EQUAL(trbuf[10], ipbuf[10]);
+    BOOST_CHECK_EQUAL(trbuf[11], ipbuf[11]);
+    BOOST_CHECK_EQUAL(trbuf[12], ipbuf[12]);
+    BOOST_CHECK_EQUAL(trbuf[13], ipbuf[13]);
+    BOOST_CHECK_EQUAL(trbuf[14], ipbuf[14]);
+    BOOST_CHECK_EQUAL(trbuf[15], ipbuf[15]);
+
+    BOOST_CHECK_EQUAL(tc.avail(), 0);
+    BOOST_CHECK_EQUAL(tc.len(), sizeof(trbuf));
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
