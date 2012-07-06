db22f89b78a6bbb6c86d1c5eceb42ee6a019c62a
Stephan Neuhaus <neuhaust@tik.ee.ethz.ch>
2012-07-06 12:45:01 +0000

    Added coverage support
    git-svn-id: https://minerva.netgroup.uniroma2.it/svn/demons/Sources/blockmon/main/node/lib/fc@2954 56ad3299-f7c0-456c-8eb5-fe9b43863049

--- test/TestTranscoder.cpp
+++ test/TestTranscoder.cpp
@@ -1,5 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 
+#include <boost/detail/endian.hpp>
+
 #include <boost/test/test_tools.hpp>
 #include <boost/test/unit_test.hpp>
 
@@ -9,9 +11,10 @@
 BOOST_AUTO_TEST_SUITE(Basics)
 BOOST_AUTO_TEST_SUITE(Transcoder)
 
-static const size_t buf_size = 130;
 
 BOOST_AUTO_TEST_CASE(BufferManagement) {
+  static const size_t buf_size = 130;
+
   IPFIX::Transcoder tc;
   uint8_t* buf = new uint8_t[buf_size];
 
@@ -63,17 +66,26 @@ BOOST_AUTO_TEST_CASE(TranscodeIPv4Address) {
 
     BOOST_CHECK_EQUAL(tc.encode(&ipbuf[0], sizeof(ipbuf), ie_sip), true);
 
+    /* Transcoding of IPv4 addresses changes byte order only
+     * on little-endian machines. */
+#if defined(BOOST_LITTLE_ENDIAN)
     BOOST_CHECK_EQUAL(trbuf[0], ipbuf[3]);
     BOOST_CHECK_EQUAL(trbuf[1], ipbuf[2]);
     BOOST_CHECK_EQUAL(trbuf[2], ipbuf[1]);
     BOOST_CHECK_EQUAL(trbuf[3], ipbuf[0]);
+#else
+    BOOST_CHECK_EQUAL(trbuf[0], ipbuf[0]);
+    BOOST_CHECK_EQUAL(trbuf[1], ipbuf[1]);
+    BOOST_CHECK_EQUAL(trbuf[2], ipbuf[2]);
+    BOOST_CHECK_EQUAL(trbuf[3], ipbuf[3]);
+#endif
 
     BOOST_CHECK_EQUAL(tc.avail(), 0);
     BOOST_CHECK_EQUAL(tc.len(), sizeof(trbuf));
   }
 }
 
-static const unsigned int n_ipv6_transcode_tests = 1;
+static const unsigned int n_ipv6_transcode_tests = 1000;
 
 BOOST_AUTO_TEST_CASE(TranscodeIPv6Address) {
   IPFIX::InfoModel m = IPFIX::InfoModel::instance();
@@ -90,8 +102,12 @@ BOOST_AUTO_TEST_CASE(TranscodeIPv6Address) {
     tc.setBase(trbuf, sizeof(trbuf));
     tc.zero();
 
-    BOOST_CHECK_EQUAL(tc.encode(&ipbuf[0], sizeof(ipbuf), ie_sip), true);
+    // Use sizeof(ip), not sizeof(ipbuf).
+    BOOST_CHECK_EQUAL(tc.encode(&ipbuf[0], sizeof(ip), ie_sip), true);
     
+    /* Transcoding of IPv4 addresses never changes the byte order
+     * because in IPv6, host byte order is big endian by
+     * definition. */
     BOOST_CHECK_EQUAL(trbuf[ 0], ipbuf[ 0]);
     BOOST_CHECK_EQUAL(trbuf[ 1], ipbuf[ 1]);
     BOOST_CHECK_EQUAL(trbuf[ 2], ipbuf[ 2]);
@@ -112,6 +128,56 @@ BOOST_AUTO_TEST_CASE(TranscodeIPv6Address) {
     BOOST_CHECK_EQUAL(tc.avail(), 0);
     BOOST_CHECK_EQUAL(tc.len(), sizeof(trbuf));
   }
+}
+
+BOOST_AUTO_TEST_CASE(Focus) {
+  static const size_t buf_size = 100;
+  static const size_t offset = 51;
+
+  uint8_t buf[buf_size];
+  IPFIX::Transcoder tc;
+
+  tc.setBase(buf, sizeof(buf));
+  tc.zero();
+
+  tc.focus(offset, buf_size - offset);
+  BOOST_CHECK_EQUAL(tc.cur(), tc.base() + offset);
+  BOOST_CHECK_EQUAL(tc.avail(), buf_size - offset);
+}
+
+BOOST_AUTO_TEST_CASE(FocusAndTranscodeOddAddress) {
+  static const size_t buf_size = 100;
+  static const size_t offset = 51;
+
+  uint8_t buf[buf_size];
+  IPFIX::Transcoder tc;
+
+  tc.setBase(buf, sizeof(buf));
+  tc.zero();
+
+  tc.focus(offset, buf_size - offset);
+  IPFIX::InfoModel m = IPFIX::InfoModel::instance();
+  m.defaultIPFIX();
+  const IPFIX::InfoElement* ie_sip = m.lookupIE("sourceIPv4Address");
+  BOOST_CHECK(ie_sip != 0);
+
+  uint32_t ip = r32();
+  
+  BOOST_CHECK_EQUAL(tc.encode(reinterpret_cast<uint8_t*>(&ip),
+                              sizeof(ip), ie_sip), true);
+
+#if defined(BOOST_LITTLE_ENDIAN)
+    BOOST_CHECK_EQUAL(buf[offset + 0], reinterpret_cast<uint8_t*>(&ip)[3]);
+    BOOST_CHECK_EQUAL(buf[offset + 1], reinterpret_cast<uint8_t*>(&ip)[2]);
+    BOOST_CHECK_EQUAL(buf[offset + 2], reinterpret_cast<uint8_t*>(&ip)[1]);
+    BOOST_CHECK_EQUAL(buf[offset + 3], reinterpret_cast<uint8_t*>(&ip)[0]);
+#else
+    BOOST_CHECK_EQUAL(buf[offset + 0], reinterpret_cast<uint8_t*>(&ip)[0]);
+    BOOST_CHECK_EQUAL(buf[offset + 1], reinterpret_cast<uint8_t*>(&ip)[1]);
+    BOOST_CHECK_EQUAL(buf[offset + 2], reinterpret_cast<uint8_t*>(&ip)[2]);
+    BOOST_CHECK_EQUAL(buf[offset + 3], reinterpret_cast<uint8_t*>(&ip)[3]);
+#endif
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
