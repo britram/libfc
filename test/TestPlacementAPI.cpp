873dcd37803d452f1fe4eca328bbe223e51928ed
Brian Trammell <trammell@tik.ee.ethz.ch>
2013-02-05 12:42:57 +0100

    tweak copyright

--- test/TestPlacementAPI.cpp
+++ test/TestPlacementAPI.cpp
@@ -7,9 +7,7 @@
  *    * Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *    * Neither the names of NEC Europe Ltd, Consorzio Nazionale 
- *      Interuniversitario per le Telecomunicazioni, Institut Telecom/Telecom 
- *      Bretagne, ETH Zürich, INVEA-TECH a.s. nor the names of its contributors 
+ *    * Neither the name of ETH Zürich, nor the names of its contributors 
  *      may be used to endorse or promote products derived from this software 
  *      without specific prior written permission.
  *
