744f1f4725a85c2d2c56140a569a86cfa1bfacba
Stephan Neuhaus <neuhaust@tik.ee.ethz.ch>
2014-04-04 13:21:22 +0200

    C binding for libfc

---
+++ cbinding.c
@@ -0,0 +1,59 @@
+/* Copyright (c) 2011-2012 ETH Zürich. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the names of ETH Zürich nor the names of other contributors 
+ *      may be used to endorse or promote products derived from this software 
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH
+ * ZÜRICH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+#include <stdint.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "ipfix.h"
+
+static uint32_t sip;
+static uint32_t dip;
+
+static void callback(const struct ipfix_template_t* p) {
+  printf("Got new values %08x and %08x\n", sip, dip);
+}
+
+int main() {
+  int fd = open("test.ipfix", O_RDONLY);
+  if (fd < 0)
+    exit(1);
+  
+  struct ipfix_template_t* t = ipfix_template_new();
+  ipfix_register_placement(t, "sourceIPv4Address", &sip, 4);
+  ipfix_register_placement(t, "destinationIPv4Address", &dip, 4);
+  ipfix_register_callback(t, callback);
+  ipfix_collect_from_file(fd, t);
+  if (close(fd) < 0)
+    exit(1);
+   
+  ipfix_template_delete(t);
+
+  return 0;
+}
---
+++ ipfix.cpp
@@ -0,0 +1,111 @@
+/* Copyright (c) 2011-2012 ETH Zürich. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the names of ETH Zürich nor the names of other contributors 
+ *      may be used to endorse or promote products derived from this software 
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH
+ * ZÜRICH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+#include "ipfix.h"
+
+#include "InfoModel.h"
+#include "PlacementTemplate.h"
+#include "PlacementCollector.h"
+#include "FileInputSource.h"
+#include "exceptions/FormatError.h"
+
+static bool infomodel_initialized = false;
+
+class CBinding : public IPFIX::PlacementCollector {
+private:
+  const struct ipfix_template_t* enclosing;
+  IPFIX::PlacementTemplate* tmpl;
+  void (*callback) (const ipfix_template_t* t);
+
+public:
+  CBinding(const struct ipfix_template_t* enclosing) 
+    : enclosing(enclosing),
+      tmpl(new IPFIX::PlacementTemplate()), callback(0) {
+  }
+
+  virtual ~CBinding() {
+    delete tmpl;
+  }
+
+  bool register_placement(const char* name, void* address, size_t size) {
+    return tmpl->register_placement(IPFIX::InfoModel::instance().lookupIE(name),
+                                    address, size);
+  }
+
+  void register_callback(void (*c) (const struct ipfix_template_t*)) {
+    callback = c;
+  }
+
+  void start_placement(const IPFIX::PlacementTemplate* tmpl) {
+  }
+
+  void end_placement(const IPFIX::PlacementTemplate* tmpl) {
+    callback(enclosing);
+  }
+};
+
+struct ipfix_template_t {
+  CBinding* binding;
+};
+
+extern struct ipfix_template_t* ipfix_template_new() {
+  if (!infomodel_initialized)
+    IPFIX::InfoModel::instance().defaultIPFIX();
+  infomodel_initialized = true;
+
+  struct ipfix_template_t* ret = new ipfix_template_t;
+  ret->binding = new CBinding(ret);
+  return ret;
+}
+
+extern void ipfix_template_delete(struct ipfix_template_t* p) {
+  delete p->binding;
+  delete p;
+}
+
+extern int ipfix_register_placement(struct ipfix_template_t* t,
+                                    const char* ie_name, void* p, size_t size) {
+  return t->binding->register_placement(ie_name, p, size);
+}
+
+extern void ipfix_register_callback(struct ipfix_template_t* t,
+                                    void (*c) (const struct ipfix_template_t*)) {
+  t->binding->register_callback(c);
+}
+
+extern int ipfix_collect_from_file(int fd, struct ipfix_template_t* t) {
+  int ret = 1;
+
+  IPFIX::FileInputSource is(fd);
+  try {
+    t->binding->collect(is);
+  } catch (IPFIX::FormatError e) {
+    std::cerr << "Format error: " << e.what() << std::endl;
+    ret = 0;
+  }
+
+  return ret;
+}
