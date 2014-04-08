8a1c936d1d9d0bd2dd478cf925da2f821fb01068
Stephan Neuhaus <neuhaust@tik.ee.ethz.ch>
2014-04-08 16:07:45 +0200

    Changed copyright notice: adapted years, changed generic <COPYRIGHT HOLDER> to ETH ZURICH.

--- ipfix.h
+++ ipfix.h
@@ -1,5 +1,5 @@
 /* Hi Emacs, please use -*- mode: C; -*- */
-/* Copyright (c) 2011-2012 ETH Zürich. All rights reserved.
+/* Copyright (c) 2011-2014 ETH Zürich. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
